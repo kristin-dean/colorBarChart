5e825b4670bb88d1e22cda2e46ec2b92448149fc
kristin-dean <47224694+kristin-dean@users.noreply.github.com>
2019-03-06 15:46:43 -0500

    Update makeChart.js

--- makeChart.js
+++ makeChart.js
@@ -42,7 +42,7 @@ svg.selectAll("text")
    .attr("x", function(d,i)
       { return (i * barWidth) + 22;})
    .attr("y", function(d)
-      { return height - (d.num*20)+18;})
+      { return height - (d.num*20)+30;})
    .attr("fill", "white")
 
 }
